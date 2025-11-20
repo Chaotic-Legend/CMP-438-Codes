eb1af9a53ba6122d6cb9d34cc0f5407598085790
Isaac Hoyos <gamemasterx40@gmail.com>
2025-11-20 11:31:05 -0500

    Correct loop increment in color setting loop
    Fix loop increment in pixel color setting.

--- NeoPixel Lab: Arduino RGB LED Strip.ino
+++ NeoPixel Lab: Arduino RGB LED Strip.ino
@@ -1,15 +1,14 @@
 // CMP 438: Communicating Robots - Isaac D. Hoyos
+
 #include <Adafruit_NeoPixel.h>
 #define PIN 12
 #define NUMPIXELS 4
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-
 int delayVal = 100;
 int redColor = 0;
 int greenColor = 0;
 int blueColor = 0;
-
 void setup() {
   pixels.begin();
   pixels.show();
@@ -17,7 +16,6 @@ void setup() {
 
 void loop() {
   setColor();
-  
   for (int i = 0; i < NUMPIXELS; i++) {
     pixels.setPixelColor(i, pixels.Color(redColor, greenColor, blueColor));
     pixels.show();
