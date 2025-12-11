51610be6981bf53b3a87639f420ec7693b06f25c
Isaac Hoyos <gamemasterx40@gmail.com>
2025-12-11 15:05:28 -0500

    Update lab link from Raspberry Pi Pico to Arduino UNO

--- Lab #1: Raspberry Pi Pico.py
+++ Lab #1: Raspberry Pi Pico.py
@@ -1,5 +1,5 @@
 # CMP 438: Communicating Robots - Isaac D. Hoyos
-# Lab 1 - LED PWM: https://wokwi.com/projects/450055821058964481
+# Lab 1 - Arduino UNO: https://wokwi.com/projects/450055789620555777
 
 import time
 from machine import Pin
